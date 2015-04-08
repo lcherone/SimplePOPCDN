d39170870f66c585f745c50f55d003644c80320f
Lawrence Cherone <lcherone@users.noreply.github.com>
2015-04-08 22:32:05 +0100

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -3,10 +3,10 @@ include '../index.php';
 
 class SimplePOPCDNTest extends PHPUnit_Framework_TestCase {
  
-  public function test()
-  {
-    $test = new SimplePOPCDN('http://server.to.mirror.com', './cache/', '/subdir', 2628000);
-    $this->assertTrue(true);
-  }
+    public function test()
+    {
+        $test = new SimplePOPCDN('http://server.to.mirror.com', './cache/', '/subdir', 2628000);
+        $this->assertTrue(true);
+    }
  
 }
